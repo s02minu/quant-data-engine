a5713e48c0ab2caf95f20f4659cd14b7e884e89f
so2minu <israeladetola@proton.me>
2026-05-25 21:14:12 +0100

    Guard against yfinacne MultiIndex update

--- src/qde/loaders/yfinance_loader.py
+++ src/qde/loaders/yfinance_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import yfinance as yf
 
-def load_ohlcv(symbol, start, end=None, interval='id'):
+def load_ohlcv(symbol, start, end=None, interval='1d'):
         data  = yf.download(
                 tickers=symbol,
                 start=start,
@@ -9,5 +9,9 @@ def load_ohlcv(symbol, start, end=None, interval='id'):
                 interval=interval,
                 auto_adjust=True,
         )
+
+        if isinstance(data.columns, pd.MultiIndex):
+                data = data.droplevel(1, axis=1)
+
         return data
 
