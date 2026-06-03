06e6928a858059b999d0558beecf5459ecf14d8d
so2minu <israeladetola@proton.me>
2026-06-03 21:52:00 +0100

    Lower Case the columns for standardization"

--- src/qde/loaders/yfinance_loader.py
+++ src/qde/loaders/yfinance_loader.py
@@ -14,11 +14,16 @@ def load_ohlcv(symbol, start, end=None, interval='1d'):
     if isinstance(data.columns, pd.MultiIndex):
         data = data.droplevel(1, axis='columns')
 
+    # LowerCasing for standardization
+    data.columns = data.columns.str.lower()
+
     # Timezone handling
     if data.index.tz is None:
         data.index = data.index.tz_localize('UTC')
+
     else:
         data.index = data.index.tz_convert('UTC')
+
     data.index.name = 'date'
 
     return data
